50c9296b663fc73810c5c9bdee1110ac979068c6
Harsh Raj <hr45226@gmail.com>
2020-10-09 19:52:28 +0530

    Updates

---
+++ precache-manifest.0838a5aa1f11477324e9aad601bc0625.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2052363aa3c24553258f59ef1f4f6d30",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "8f0f67b2bc5be08f935e",
+    "url": "/my-portfolio/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "91aa67a170b0965cb46f",
+    "url": "/my-portfolio/static/css/main.502e2e0d.chunk.css"
+  },
+  {
+    "revision": "8f0f67b2bc5be08f935e",
+    "url": "/my-portfolio/static/js/2.35050ff6.chunk.js"
+  },
+  {
+    "revision": "7c31314e008d1a59ddfae5300ece5508",
+    "url": "/my-portfolio/static/js/2.35050ff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91aa67a170b0965cb46f",
+    "url": "/my-portfolio/static/js/main.c7b400ac.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "92a69a23d67f2cc96e91050baedb9d11",
+    "url": "/my-portfolio/static/media/e.92a69a23.png"
+  },
+  {
+    "revision": "7719b4a6dba615cb5912758aa2f15471",
+    "url": "/my-portfolio/static/media/i.7719b4a6.png"
+  },
+  {
+    "revision": "b46123cfe6719777c41e732e47ba6261",
+    "url": "/my-portfolio/static/media/logo.b46123cf.png"
+  },
+  {
+    "revision": "a024d12892baa2ce5bd5bc89615316cb",
+    "url": "/my-portfolio/static/media/p1.a024d128.png"
+  },
+  {
+    "revision": "9bf8ffb7f333f5496ea31b8cf1f0f3fa",
+    "url": "/my-portfolio/static/media/r.9bf8ffb7.png"
+  },
+  {
+    "revision": "b1a36a7f695768bc02cdc98557246aef",
+    "url": "/my-portfolio/static/media/resu.b1a36a7f.jpg"
+  },
+  {
+    "revision": "310731e4521bc6f362c379a0b808ff3f",
+    "url": "/my-portfolio/static/media/s2.310731e4.png"
+  },
+  {
+    "revision": "12be439d977ccc965d9f6eccbeeb0dce",
+    "url": "/my-portfolio/static/media/soc.12be439d.png"
+  }
+]);
